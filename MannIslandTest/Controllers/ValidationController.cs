256176bcfc1f006585039c477a7a83170d52ae83
adamshort1977 <adam.short@gmail.com>
2018-10-17 17:58:07 +0100

    initial commit

---
+++ MannIslandTest/Controllers/ValidationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace MannIslandTest.Controllers
+{
+    public class ValidationController : Controller
+    {
+        public JsonResult Validate(string sortcode, string accountnumber)
+        {
+            var test = "this is a test";
+            return new JsonResult(test);
+        }
+    }
+}
